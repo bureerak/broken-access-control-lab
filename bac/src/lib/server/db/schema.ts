3d21ad260208df912d4eec5ebba85a47c7f41f61
bureerak <pongphon_brr@hotmail.com>
2025-11-06 13:02:40 +0700

    Set up

---
+++ bac/src/lib/server/db/schema.ts
@@ -0,0 +1,8 @@
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+
+export const user = sqliteTable('user', {
+	id: text('id').primaryKey(),
+	name: text('name'),
+	email: text('email').unique(),
+	password: text('password'),
+});
